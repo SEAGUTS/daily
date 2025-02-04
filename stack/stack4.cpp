d3e5d45eed98cde80d2de5b2bfe9a2caf9cada49
Shreyansh Golchha <shreyanshgolchha1112@gmail.com>
2025-02-04 09:36:11 +0530

    Add files via upload

--- stack/stack4.cpp
+++ stack/stack4.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <stack>
 using namespace std;
-void solve(stack <int> &s,int count,int N)
+void solve(stack<int> &stk,int count,int N)
 {
     if (count == N/2)
     {
-        s.pop();
+        stk.pop();
         return;
     }
-    int num = s.top();
-    s.pop();
-    solve(s,count+1,N);
+    int num = stk.top();
+    stk.pop();
+    solve(stk,count+1,N);
+    stk.push(num);
 }
-void deletemid(stack <int> &s,int N)
+void deletemid(stack <int> &stk,int N)
 {
     int count = 0;
-    solve(s,count,N);
+    solve(stk,count,N);
 }
 int main()
 {
@@ -27,7 +28,11 @@ int main()
     s.push(5);
     s.push(6);
     s.push(7);
-    deletemid(s,s.size());
-    cout<<s.size();
+    deletemid(s,7);
+    for (int i = 0; i < 7; i++)
+    {
+        cout<<s.top();
+        s.pop();
+    }
     return 0;
 }
